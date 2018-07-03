2405bf21904bce797755243b94b7e45afd1be94d
Kevin De Koninck <kevin.de.koninck92@gmail.com>
2018-07-03 20:48:41 +0200

    Reduce Docker image size
    https://github.com/vincentcox/StaCoAn/issues/70

--- docker/stacoan.sh
+++ docker/stacoan.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-pwd
-ls -la
-ls -la /
-python3 /StaCoAn/src/stacoan.py --disable-browser --enable-server
+python3 /StaCoAn/stacoan.py --disable-browser
 
 # https://stackoverflow.com/questions/90418/exit-shell-script-based-on-process-exit-code
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 
 # Redirect to start.html
-echo "<script>window.location = 'http://'+window.location.hostname+':8080'+'/start.html';</script>" > /StaCoAn/src/report/index.html
-cd /StaCoAn/src/report/ && python -m http.server 8080
+echo "<script>window.location = 'http://'+window.location.hostname+':8080'+'/start.html';</script>" > /StaCoAn/report/index.html
+cd /StaCoAn/report/ && python -m http.server 8080
